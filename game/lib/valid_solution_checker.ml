88178def5db1a26039da6dad29a1cfce27c65063
Darren Key <dyk34@cornell.edu>
2022-03-02 00:50:20 -0500

    Tests recognize game

--- game/lib/valid_solution_checker.ml
+++ game/lib/valid_solution_checker.ml
@@ -1,10 +1,34 @@
-type operator = 
-| Multiplication
-| Division
-| Addition
-| Subtraction
+type operator =
+  | Multiplication
+  | Division
+  | Addition
+  | Subtraction
 
-type tree = 
-| Leaf of int
-| Node of operator * tree * tree
+type tree =
+  | Leaf of int
+  | Node of operator * tree * tree
 
+let check_valid_char (_ : string) =
+  raise (Failure "Unimplemented: Main.play_game")
+
+let check_valid_paren (_ : string) =
+  raise (Failure "Unimplemented: Main.play_game")
+
+let check_valid_operations (_ : string) =
+  raise (Failure "Unimplemented: Main.play_game")
+
+let check_all_nums_used_once
+    (_ : string)
+    (_ : int * int * int * int) =
+  raise (Failure "Unimplemented: Main.play_game")
+
+let check_solution_valid
+(_ : string)
+(_ : int * int * int * int) =
+raise (Failure "Unimplemented: Main.play_game")
+
+let expression_tree_creator (_ : string) =
+  raise (Failure "Unimplemented: Main.play_game")
+
+let check_expression_tree (_ : tree) =
+  raise (Failure "Unimplemented: Main.play_game")
--- game/test/main.ml
+++ game/test/main.ml
@@ -1,6 +1,6 @@
 open OUnit2
 open Game
 
+let valid_solution_tests = []
 let suite = "test suite for 24 battle royale" >::: List.flatten []
-
 let _ = run_test_tt_main suite
