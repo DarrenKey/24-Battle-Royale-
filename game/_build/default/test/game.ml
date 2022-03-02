0507936ddbdfd95b113376478f2bbc2690c68e9d
Jason Guo <sg763@cornell.edu>
2022-03-01 22:17:27 -0500

    Set up dune for lib

--- game/_build/default/test/game.ml
+++ game/_build/default/test/game.ml
@@ -1,1 +1,6 @@
 open OUnit2
+open Game
+
+let suite = "test suite for 24 battle royale" >::: List.flatten []
+
+let _ = run_test_tt_main suite
--- game/test/game.ml
+++ game/test/game.ml
@@ -1,4 +1,5 @@
 open OUnit2
+open Game
 
 let suite = "test suite for 24 battle royale" >::: List.flatten []
 
