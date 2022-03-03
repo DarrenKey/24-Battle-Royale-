23c5b003c98712ffde7759192758ce6d340ee5b6
Jason Guo <sg763@cornell.edu>
2022-03-02 22:44:47 -0500

    Implement in_set
    - add test cases
    - add helper function to convert list to hashset

--- game/lib/combinations.ml
+++ game/lib/combinations.ml
@@ -7,17 +7,26 @@ module Combinations = struct
   (** [seen_combs] is the set containing all combinations that have been generated*)
   let seen_combs num_combs = Hashtbl.create num_combs
 
-  (** [list_to_tuple] converts [lst] of 4 numbers to tuple*)
+  (** [list_to_tuple] converts [lst] of 4 numbers to tuple
+      Example: [list_to_tuple \[1;2;3;4\]] is [(1,2,3,4)]
+      Requires: lst is a list of 4 elements
+      Raises: Invalid_argument *)
   let list_to_tuple lst = match lst with
   | a::b::c::d::[] -> (a, b, c, d)
   | _ -> raise (Invalid_argument "[lst] is not a list of 4 numbers")
 
-  (** [in_set] is whether or not [lst] of 4 numbers is in [tbl]*)
+  (** [in_set] is whether or not the combination of 4 numbers represented by [lst] is in [tbl] 
+      Example: [in_set {(1,2,3,4)} \[2;3;1;4\]]
+      Requires: lst is a list of 4 numbers
+      Raises: Invalid_argument *)
   let in_set tbl lst = match lst with
   | _::_::_::_::[] -> List.sort compare lst |> list_to_tuple |> Hashtbl.mem tbl 
   | _ -> raise (Invalid_argument "[lst] is not a list of 4 numbers")
   
-  (** [isvalid_comb] is true iff you can get 24 with the four numbers in comb*)
+  (** [is_valid_comb] is true iff you can get 24 with the four numbers in list [comb]
+      Example: [is_valid_comb \[1;1;4;6\]] is true
+      Requires: comb is a list of 4 positive numbers less than or equal to 12
+      Raises: Invalid_argument *)
   let is_valid_comb comb = failwith "not implemented"
 
 end
--- game/test/main.ml
+++ game/test/main.ml
@@ -5,12 +5,22 @@ open Valid_solution_checker
 
 let valid_solution_tests = []
 
-let ht = Hashtbl.create 10
+(** [hashset] is the hash set of [lst] implemented with a Hashtbl
+    Example: [hashset \[1;2;3;4\]] is {1,2,3,4} *)
+let hashset lst = let empty_set = Hashtbl.create 10 in 
+  let rec add_to_tbl tbl processing_list = match processing_list with 
+    | [] -> tbl
+    | h::tail -> Hashtbl.add tbl h false; add_to_tbl tbl tail
+  in add_to_tbl empty_set lst
+
 let in_set_test name tbl lst expected = 
-  name >:: fun _ -> (assert_equal expected (in_set tbl lst))
+  name >:: fun _ -> (assert_equal expected (in_set tbl lst));;
 
 let comb_test = [
-  in_set_test "[1,2,3,4] is not in the empty set" ht [1;2;3;4] false
+  in_set_test "[1;2;3;4] is not in the empty set" (hashset []) [1;2;3;4] false;
+  in_set_test "[1;2;3;4] is in the set {(1,2,3,4)}" (hashset [(1,2,3,4)]) [1;2;3;4] true;
+  in_set_test "[14;13;20;10] is not in the set {(1,2,3,4),(5,6,7,8)}" (hashset [(1,2,3,4);(5,6,7,8)]) [14;13;20;10] false;
+  in_set_test "[4;2;3;1] is in the set {(1,2,3,4),(5,6,7,8)}" (hashset [(1,2,3,4); (5,6,7,8)]) [4;2;3;1] true;
 ]
 
 let suite = "test suite for 24 battle royale" >::: List.flatten [comb_test]
