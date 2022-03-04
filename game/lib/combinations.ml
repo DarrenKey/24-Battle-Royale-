f8ce5ee29aa7a4e1663b997c00631a9282b7cb0b
Jason Guo <sg763@cornell.edu>
2022-03-03 19:43:09 -0500

    Implement makes_24
    - add function for generating list permutations
    - add function for checking operations
    - add function for inserting an item into a list
    - add function for checking if a list has 4 elements

--- game/lib/combinations.ml
+++ game/lib/combinations.ml
@@ -27,36 +27,53 @@ module Combinations = struct
   | _::_::_::_::[] -> true
   | _ -> false
 
-    let insert item index lst = if index < 0 || index > List.length lst then 
-        raise (Failure "i is not in range of the list")
-        else 
-            let rec insert_wrapper i before n l = match i, l with
-                | 0, after -> (List.rev before) @ (n::after)
-                | ind, h::tail -> insert_wrapper (i-1) (h::before) n tail
-                | _, after -> raise (Failure "shouldn't happen")
-            in insert_wrapper index [] item lst    
+  (** [insert]s item into [lst] at [index]. [index] is where [item] would be after the insertion *)
+  let insert item index lst = 
+    if index < 0 || index > List.length lst then 
+      raise (Failure "i is not in range of the list")
+    else 
+      let rec insert_wrapper i before n l = match i, l with
+      | 0, after -> (List.rev before) @ (n::after)
+      | ind, h::tail -> insert_wrapper (i-1) (h::before) n tail
+      | _, after -> raise (Failure "shouldn't happen")
+      in insert_wrapper index [] item lst    
 
-  let rec permute_one num perm = match perm with
+  let distribute item lst = 
+    let rec distribute_wrapper n l i acc = match i with 
+    | -1 -> acc
+    | ind -> distribute_wrapper n l (i-1) ((insert n ind l)::acc)
+  in distribute_wrapper item lst (List.length lst) []
+
+  let rec permute_one num lst = match lst with
   | [] -> []
-  | h::tail -> 
+  | h::tail -> (distribute num h)@(permute_one num tail)
 
   let rec permute lst = match lst with
   | [] -> [[]]
-  | h::tail -> 
+  | h::tail -> (permute_one h (permute tail))
 
-    let rec check_ops num acc comb = match comb with
-    | [] -> acc = num
-    | h::tail -> check_ops num (acc +. h) tail 
-      || check_ops num (acc -. h) tail
-      || check_ops num (h -. acc) tail
-      || check_ops num (acc *. h) tail
-      || check_ops num (acc /. h) tail
-      || check_ops num (h /. acc) tail
+  let rec check_ops_wrapper num acc comb = match comb with
+  (* TODO: fix the floating point approximation bug!! *)
+  | [] -> (*(if 23.99 < acc && acc < 24.01 then print_endline (string_of_float acc));*) acc = num
+  | h::tail -> check_ops_wrapper num (acc +. h) tail 
+    || check_ops_wrapper num (acc -. h) tail
+    || check_ops_wrapper num (h -. acc) tail
+    || check_ops_wrapper num (acc *. h) tail
+    || check_ops_wrapper num (acc /. h) tail
+    || check_ops_wrapper num (h /. acc) tail
+
+  let check_ops num comb = match List.map float_of_int comb with
+  | h::tail -> check_ops_wrapper (float_of_int num) h tail
+  | _ -> raise (Invalid_argument "comb must be a list of 4 elements")
 
   (** [makes_24] is true iff you can get 24 with the four numbers in list [comb]
       Example: [makes_24 \[1;1;4;6\]] is true
       Requires: comb is a list of 4 positive numbers less than or equal to 12
       Raises: Invalid_argument *)
-  let makes_24 comb = arr_makes_24 (Array.of_list comb)
+  let makes_24 comb = 
+    let rec makes_24_wrapper permutations = match permutations with
+    | [] -> false
+    | h::tail -> (check_ops 24 h) || makes_24_wrapper tail
+  in makes_24_wrapper (permute comb)
     
 end
--- game/test/main.ml
+++ game/test/main.ml
@@ -16,11 +16,27 @@ let hashset lst = let empty_set = Hashtbl.create 10 in
 let in_set_test name tbl lst expected = 
   name >:: fun _ -> (assert_equal expected (in_set tbl lst));;
 
+let insert_test name item index lst expected = 
+  name >:: fun _ -> (assert_equal expected (insert item index lst))
+
+let distribute_test name item lst expected = 
+  name >:: fun _ -> (assert_equal expected (distribute item lst))
+
 let comb_test = [
   in_set_test "[1;2;3;4] is not in the empty set" (hashset []) [1;2;3;4] false;
   in_set_test "[1;2;3;4] is in the set {(1,2,3,4)}" (hashset [(1,2,3,4)]) [1;2;3;4] true;
   in_set_test "[14;13;20;10] is not in the set {(1,2,3,4),(5,6,7,8)}" (hashset [(1,2,3,4);(5,6,7,8)]) [14;13;20;10] false;
   in_set_test "[4;2;3;1] is in the set {(1,2,3,4),(5,6,7,8)}" (hashset [(1,2,3,4); (5,6,7,8)]) [4;2;3;1] true;
+
+  insert_test "insert 10 into [1;2;3;4] at index 0" 10 0 [1;2;3;4] [10;1;2;3;4];
+  insert_test "insert 10 into [1;2;3;4] at index 1" 10 1 [1;2;3;4] [1;10;2;3;4];
+  insert_test "insert 10 into [1;2;3;4] at index 2" 10 2 [1;2;3;4] [1;2;10;3;4];
+  insert_test "insert 10 into [1;2;3;4] at index 4" 10 4 [1;2;3;4] [1;2;3;4;10];
+  insert_test "insert 10 into [] at index 0" 10 0 [] [10];
+
+  distribute_test "distribute 4 into [1;2;3]" 4 [1;2;3] [[4;1;2;3];[1;4;2;3];[1;2;4;3];[1;2;3;4]];
+  distribute_test "distribute 4 into [1]" 4 [1] [[4;1];[1;4]];
+  distribute_test "distribute 4 into []" 4 [] [[4]];
 ]
 
 let suite = "test suite for 24 battle royale" >::: List.flatten [comb_test]
