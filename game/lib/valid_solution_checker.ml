d9881be1c0a75a3e8b60ed76ad44598f0d5db23c
Darren Key <dyk34@cornell.edu>
2022-03-04 18:50:26 -0500

    Fraction module added

--- game/lib/valid_solution_checker.ml
+++ game/lib/valid_solution_checker.ml
@@ -1,33 +1,3 @@
-module Fraction = struct
-  type frac = int * int
-
-  let rec gcd a b = if b = 0 then a else gcd b (a mod b)
-
-  let reduce_frac (n : frac) =
-    match n with
-    | a, b ->
-        let gcd_frac = gcd a b in
-        (a / gcd_frac, b / gcd_frac)
-
-  (* a + b *)
-  let add_frac (a : frac) (b : frac) =
-    match (a, b) with
-    | (a1, a2), (b1, b2) -> reduce_frac ((a1 * b2) + (b1 * a2), a2 * b2)
-
-  (* a - b *)
-  let subtract_frac (a : frac) (b : frac) =
-    match (a, b) with
-    | (a1, a2), (b1, b2) -> reduce_frac ((a1 * b2) - (b1 * a2), a2 * b2)
-
-  let multiply_frac (a : frac) (b : frac) =
-    match (a, b) with
-    | (a1, a2), (b1, b2) -> reduce_frac (a1 * b1, a2 * b2)
-
-  let divide_frac (a : frac) (b : frac) =
-    match (a, b) with
-    | (a1, a2), (b1, b2) -> reduce_frac (a1 * b2, a2 * b1)
-end
-
 type operator =
   | Multiplication
   | Division
